90bc801b634008d6955a71b4aaa2c5514ea8ec84
Akulk7 <anuragk@vt.edu>
2024-03-20 20:25:36 -0400

    init

---
+++ index.js
@@ -0,0 +1,172 @@
+const db = require('./templates/assets/js/dbConnect.js')
+const express = require('express')
+const session = require('express-session')
+const path = require('path')
+
+const app = express()
+const router = express.Router()
+const port = 3000
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json())
+app.use(session({
+  secret: "0312113451e14d28b67fa225e9e4e94d",
+  resave: false,
+  saveUninitialized: true
+}))
+app.use('/', router)
+app.use(express.static(path.join(__dirname, 'templates')))
+
+
+router.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'templates', 'login.html'));
+})
+
+router.get('/dashboard/student', (req, res) => {
+  res.sendFile(path.join(__dirname, 'templates', 'Student Dashboard.html'));
+})
+
+router.get('/dashboard/professor', (req, res) => {
+  res.sendFile(path.join(__dirname, 'templates', 'Professor Dashboard.html'));
+})
+
+router.post('/validateLogin', (req, res) => {
+  if (req.body.type === 'student') {
+    var test = db.validateStudent(req.body.email, req.body.password).then(isValid => {
+      if (isValid) {
+        req.session.isAuthorized = true
+        req.session.email = req.body.email
+        res.redirect('/dashboard/student')
+      }
+      else
+        res.redirect('/')
+    })
+  }
+  else {
+    var test = db.validateProfessor(req.body.email, req.body.password).then(isValid => {
+      if (isValid) {
+        req.session.isAuthorized = true
+        req.session.email = req.body.email
+        res.redirect('/dashboard/professor')
+      }
+      else
+        res.redirect('/')
+    })
+  }
+})
+
+router.get('/getCurStudent', (req, res) => {
+  db.findStudentByEmail(req.session.email).then(result => {
+    res.json({ record: result })
+  })
+})
+
+router.get('/getCurProfessor', (req, res) => {
+  db.findProfessorByEmail(req.session.email).then(result => {
+    res.json({ record: result })
+  })
+})
+
+router.get('/getAllStudents', (req, res) => {
+  res.send(String(db.selectAllStudents()))
+})
+
+router.post('/getCourses', (req, res) => {
+  if (req.body.type === 'student') {
+    db.getCoursesByStudentId(req.body.studentid).then(courses => {
+      res.json(courses);
+    })
+      .catch(err => {
+        console.error('Error retrieving courses:', err);
+        res.status(500).send('Internal server error');
+      });
+  }
+  else {
+    db.getCoursesByProfessorId(req.body.professorid).then(courses => {
+      res.json(courses);
+    })
+      .catch(err => {
+        console.error('Error retrieving courses:', err);
+        res.status(500).send('Internal server error');
+      });
+  }
+});
+/*
+git remote add origin https://github.com/anukulka/smu
+git add .
+git commit -m "Initial commit"
+git branch -M main
+git push -u origin main*/
+router.get('/isAuthorized', (req, res) => {
+  res.json({ authorized: req.session.isAuthorized ? req.session.isAuthorized : false })
+})
+
+router.get('/signout', (req, res) => {
+  req.session.destroy()
+  res.redirect('/')
+})
+/*----------------------------------------------------------------------------------------*/
+
+router.get('/forgotten-password', (req, res) => {
+  res.sendFile(path.join(__dirname, 'templates', 'forgotten-password.html'));
+});
+
+router.post('/forgot-password', async (req, res) => {
+  const { email } = req.body;
+
+  try {
+    // Check if the user exists in the database
+    const user = await db.findUserByEmail(email);
+
+    if (!user) {
+      return res.status(404).send('User not found');
+    }
+
+    // Generate a unique password reset token
+    const resetToken = crypto.randomBytes(20).toString('hex');
+
+    // Store the reset token and expiration time in the database
+    await db.storeResetToken(user.id, resetToken, Date.now() + 3600000); // Token expires in 1 hour
+
+    // Create a transporter for sending the email
+    const transporter = nodemailer.createTransport({
+      // Configure your email service provider (e.g., Gmail, SendGrid, etc.)
+      service: 'Gmail',
+      auth: {
+        user: 'your-email@gmail.com',
+        pass: 'your-email-password',
+      },
+    });
+
+    // Compose the email message
+    const mailOptions = {
+      from: 'your-email@gmail.com',
+      to: email,
+      subject: 'Password Reset',
+      text: `You are receiving this email because you (or someone else) have requested a password reset for your account.\n\n
+        Please click on the following link, or paste it into your browser to complete the process:\n\n
+        http://${req.headers.host}/reset-password/${resetToken}\n\n
+        If you did not request this, please ignore this email and your password will remain unchanged.\n`,
+    };
+
+    // Send the email
+    await transporter.sendMail(mailOptions);
+
+    res.status(200).send('Password reset email sent');
+  } catch (error) {
+    console.error('Error sending password reset email:', error);
+    res.status(500).send('Internal server error');
+  }
+});
+
+
+
+
+
+
+
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`)
+})
+
---
+++ templates/assets/js/dbConnect.js
@@ -0,0 +1,127 @@
+const { Pool } = require('pg');
+//Database connection details
+
+const pool = new Pool({
+    user: 'postgres',
+    host: 'smuproduction1.c1q46o8y6ls2.us-east-1.rds.amazonaws.com',
+    database: 'postgres',
+    username: 'postgres',
+    password: 'password',
+    port: 5432
+});
+
+//testing query for successful connection
+module.exports = {
+    selectAllStudents: function () {
+        pool.query('Select * FROM student;', (err, res) => {
+            console.log(err, res);
+            pool.end();
+        })
+    },
+
+    validateStudent: function (email, password) {
+        return new Promise((resolve, reject) => {
+            pool.query(`SELECT COUNT(*) FROM student WHERE email = '${email}' AND password = '${password}';`)
+                .then(res => {
+                    resolve(parseInt(res.rows[0].count) == 1);
+                })
+                .catch(err => {
+                    console.log(err);
+                    reject(err);
+                });
+        });
+    },
+    validateProfessor: function (email, password) {
+        return new Promise((resolve, reject) => {
+            pool.query(`SELECT COUNT(*) FROM professor WHERE email = '${email}' AND password = '${password}';`)
+                .then(res => {
+                    resolve(parseInt(res.rows[0].count) == 1);
+                })
+                .catch(err => {
+                    console.log(err);
+                    reject(err);
+                });
+        });
+    },
+    findStudentByEmail: function (email) {
+        return new Promise((resolve, reject) => {
+            pool.query(`SELECT * FROM student WHERE email = '${email}';`)
+                .then(res => {
+                    resolve(res.rows[0]);
+                })
+                .catch(err => {
+                    console.log(err);
+                    reject(err);
+                });
+        });
+    },
+
+    findProfessorByEmail: function (email) {
+        return new Promise((resolve, reject) => {
+            pool.query(`SELECT * FROM professor WHERE email = '${email}';`)
+                .then(res => {
+                    resolve(res.rows[0]);
+                })
+                .catch(err => {
+                    console.log(err);
+                    reject(err);
+                });
+        });
+    },
+
+
+    getCoursesByStudentId: function (studentid) {
+        return new Promise((resolve, reject) => {
+            pool.query(`SELECT * FROM class c
+                      JOIN Student_class sc ON c.ClassID = sc.ClassID AND c.Semester = sc.Semester AND
+                      c.Section = sc.Section
+                      WHERE sc.StudentID = '${studentid}';`)
+                .then(res => {
+                    resolve(res.rows);
+                })
+                .catch(err => {
+                    console.log(err);
+                    reject(err);
+                });
+        });
+    },
+
+    getCoursesByProfessorId: function (professorid) {
+        return new Promise((resolve, reject) => {
+            pool.query(`SELECT * FROM class 
+                      WHERE professorid = '${professorid}';`)
+                .then(res => {
+                    resolve(res.rows);
+                })
+                .catch(err => {
+                    console.log(err);
+                    reject(err);
+                });
+        });
+    },
+
+
+
+    // findUserByEmail: function(email){
+
+    // }
+    // forgotPassword: function (req, res) {
+    //     const { email } = req.body;
+
+    //     db.findUserByEmail(email)
+    //         .then(user => {
+    //             if (user) {
+    //                 const resetToken = crypto.randomBytes(20).toString('hex');
+
+    //                 return db.storeResetToken(user.id, resetToken, Date.now() + 3600000)
+    //                     .then(() => sendPasswordResetEmail(email, resetToken));
+    //             }
+
+    //             res.status(200).send('If the email exists, a password reset email has been sent.');
+    //         })
+    //         .catch(error => {
+    //             console.error('Error sending password reset email:', error);
+    //             res.status(500).send('Internal server error');
+    //         });
+    // }
+}
